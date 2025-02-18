e025c53231272f70f9adddcdc0cc0d2e575af239
Aleksandr Utbanov <a.utbanov@tbank.ru>
2025-02-18 12:48:18 +0300

    Added React journey to React-lifecycle

--- 2024-2025/29-01-2025/ReactAppWithState/src/components/TodoCardsList.jsx
+++ 2024-2025/29-01-2025/ReactAppWithState/src/components/TodoCardsList.jsx
@@ -3,22 +3,26 @@ import TodoCard from "./TodoCard";
 import PrettyLoader from "./PrettyLoader";
 
 function TodoCardsList(props) {
+    function handleRemove() {
+        // confirm - это JS-функция, которая в бразуере 
+        // вызовет окно подтверждения действия
+        const confirmed = confirm('Вы действительно хотите удалить?');
+        if (confirmed) props.onRemove(todo.id);
+    }
+
     return (
         <div className="todoCardsList">
             {props.loading ? <PrettyLoader /> : (
                 <ul className="todoItems">
                     {props.todoList.length === 0 && 
-                        'Список пусто. Создайте для себя ToDo'
+                        <p>Список пусто. Создайте для себя ToDo</p>
                     }
 
                     {props.todoList.map((todo, i) => 
                         <TodoCard 
                             key={i} 
                             content={todo.content}
-                            onRemove={function() {
-                                const confirmed = confirm('Вы действительно хотите удалить?');
-                                if (confirmed) props.onRemove(todo.id);
-                            }}
+                            onRemove={handleRemove}
                         />
                     )}
                 </ul>
